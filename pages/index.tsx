545259bb557eedc647b9e023eca37e168d6e0366
Mery <nasibajahon1995@gmail.com>
2026-09-14 17:08:00 +0900

    feat: develop mobile version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,29 @@
-
 import Advertisement from "@/libs/components/homepage/Advertisement";
 import PopularProperties from "@/libs/components/homepage/PopularProperties";
 import TopAgents from "@/libs/components/homepage/TopAgents";
 import TopProperties from "@/libs/components/homepage/TopProperties";
 import TrendProperties from "@/libs/components/homepage/TrendProperties";
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-
-import { useQuery } from "@apollo/client";
+import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
 import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <Stack className={"home-page"}>
-      <TrendProperties />
-      <PopularProperties />
-      <Advertisement />
-      <TopProperties />
-      <TopAgents />
-    </Stack>
-  );
+  const device = useDeviceDetect();
+
+  if (device === "mobile") {
+    return <Stack>HOMEPAGE MOBILE</Stack>;
+  } else {
+    return (
+      <Stack className={"home-page"}>
+        <TrendProperties />
+        <PopularProperties />
+        <Advertisement />
+        <TopProperties />
+        <TopAgents />
+      </Stack>
+    );
+  }
 };
 
 export default withLayoutMain(Home);
